837cee067df0290a31ce5af8402330dde50d5d64
Gbenga Olufemi <phemmylincon27@gmail.com>
2019-12-17 10:07:01 -0600

    not done

--- balancedBrackets/prompt.js
+++ balancedBrackets/prompt.js
@@ -9,4 +9,22 @@
 // str: "[{()}]"                  |   true
 // str: "(50)("                   |   false
 // str: "[{]}"                    |   false
-const isBalanced = str => {};
+const isBalanced = str => {
+    // describe the open and close ssets of parenthesis, curly brackets and square brackets
+    //  describe the pairs of curly brackets, parenthesis, and square brackets
+    const open = new Set(['{','[', '('])
+    const close = new Set(['}', ']', ')'])
+    const pair = { '{':'}', '[':']', '(': ')' }
+    //  declare a new variable with an empty array for storage
+    const result= [];
+    if(str === ''){
+        return true
+    } else if(close.has(next) && pairs[result.pop()] !== next){
+        return false
+    }
+};
+
+
+
+
+
