8c6d0e9ced983ebe5a1cd14995b2364263db97e4
Gbenga Olufemi <phemmylincon27@gmail.com>
2019-12-19 11:26:07 -0600

    maxConsecutiveOnes

--- maxConsecutiveOnes/prompt.js
+++ maxConsecutiveOnes/prompt.js
@@ -3,5 +3,21 @@
 //maxConsecutiveOnes([1,1,2,0,3,1,1,1,1,5,6,3]) returns 4
 
 function maxConsecutiveOnes(array) {
-  //write solution code
+  let count = 0;
+  let result = 0;
+  //loop through the array
+  for(let i = 0; i < array.length; i++){
+    // reset count whenever 0 is found in the array
+    if(array[i]=== 0){
+      count = 0;
+      // if 1 is found increment count and update the result 
+    }else if( array[i]=== 1){
+        count++;
+    }
+  }
+     return result;
 }
+
+
+
+
