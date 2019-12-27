84e4c89358ba126119212b687bf78a7c5fca7e84
Gbenga Olufemi <phemmylincon27@gmail.com>
2019-12-26 23:39:54 -0600

    longest palindrome solution

--- longestPalindrome/prompt.js
+++ longestPalindrome/prompt.js
@@ -6,4 +6,11 @@
  * whitespace on each side of dad).
  */
 
-var longestPalindrome = function(string) {};
+function longestPalindrome(str) {
+    for (i = 0; i < str.length; i++) {
+          if (str[i] !== str[str.length - 1 ]) {
+        return false;
+      }
+    }
+  return true;
+  }
