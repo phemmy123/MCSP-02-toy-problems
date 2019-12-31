389feb5ab42fa804317279329a34b59cd8fd7975
Gbenga Olufemi <phemmylincon27@gmail.com>
2019-12-30 21:18:47 -0600

    solution to nonRepeatedCharacter

--- nonRepeatedCharacter/prompt.js
+++ nonRepeatedCharacter/prompt.js
@@ -6,6 +6,28 @@
  *   firstNonRepeatedCharacter('AACBDB'); // => 'C'
  */
 
-var firstNonRepeatedCharacter = function(string) {
-  // TODO: your solution here
-};
+// var firstNonRepeatedCharacter = function(string) {
+//   // TODO: your solution here
+// };
+function firstNonRepeatedCharacter(str) {
+  // declare a variable and set it equal to an empty array
+  let count = []
+  // declare another variable and set it equal to zero
+  let newCount = 0;
+  // make a for loop to loop through the array
+  for (let i = 0; i < str.length; i++) {
+    //  make a conditional statement
+      if (!count[str.charAt(i)]) {
+          count[str.charAt(i)] = newCount + 1;
+      }
+      else {
+          count[str.charAt(i)] = count[str.charAt(i)] + 1;
+      }
+  }
+  for (let key in count) {
+      if (count[key] === 1) {
+          return key;
+      }
+  }
+  return null;
+}
