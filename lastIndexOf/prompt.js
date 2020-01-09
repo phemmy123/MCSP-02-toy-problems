ea2af402cb1f0986844da25980741935520d69e6
Gbenga Olufemi <phemmylincon27@gmail.com>
2020-01-09 10:42:32 -0600

    lastIndex

--- lastIndexOf/prompt.js
+++ lastIndexOf/prompt.js
@@ -1,6 +1,7 @@
 // Write a function that returns the last index that a given element is found
 // Ex: lastIndexOf(4, [2,3,6,4,7,4,9]) returns 5
 
-function lastIndexOf(target, array) {
-  //solution code here
+function last(target, array){
+  var result = last.lastIndexOf(array.length-1);
+  return result;
 }
