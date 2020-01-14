7f4e55523bf2596a6b17c712ec69a49c487d66cf
Gbenga Olufemi <phemmylincon27@gmail.com>
2020-01-14 10:02:43 -0600

    boggle

--- boggleSolve/prompt.js
+++ boggleSolve/prompt.js
@@ -3,4 +3,13 @@
 // * i.e. a=1, z=26 etc.
 // return the sum of the values of each letter in string
 
-const boggleSolve = str => {};
+
+// create a variable of the string alphabeth by putting all the letters in a string
+var boggleSolver = (str)=>{
+var alphabet = 'a, b, c...z'
+// create a variable letters
+var letters = str.split('')
+return letters.reduce((sum, i)=>{
+    return alphabet.indexOf(letters[i])
+})
+}
